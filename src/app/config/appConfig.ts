00b4dae217a8a71787e7b139988aeb49fb140b5e
Vinicius <viniciusmuniz4002@gmail.com>
2025-03-30 16:12:27 -0300

    Resvolvendo bugs

--- src/app/config/appConfig.ts
+++ src/app/config/appConfig.ts
@@ -1,6 +1,6 @@
 export const getUrl = () => {
     const school = {
-        host: "https://api-user-jade.vercel.app/api/"
+        host: "https://api-user-jade.vercel.app/api"
     }
     return school.host;
 
